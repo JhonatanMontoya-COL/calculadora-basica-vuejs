052d64fe357e867876383448f0b6d06a445a98e7
jhonatanmontoya <jamm29sep2005@gnail.com>
2024-10-30 16:57:01 -0500

    errores borrar

--- api fetch/api-fetch.js
+++ api fetch/api-fetch.js
@@ -2,8 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
       dataUser:[],
-      fila:0,
+      fila:0
     },
+    
     methods: {
         getUser(){
             const user = 'https://randomuser.me/api/';
@@ -11,10 +12,26 @@ var app = new Vue({
             .then(response=>{return response.json();})
             .then(data=>{this.dataUser=this.dataUser.concat(data.results) || [];})
             .catch(userError=>{console.log('el sistema ha tenido un error al leer el usuario',userError)})
-        },
-        removeUser(item) {
-          this.dataUser.splice(item,1);
-        }
+            this.fila = this.fila + 1;
+          },
+          addNumberBtn(){
+          console.log(this.fila)
+
+          },
+          removeBtn(){
+            console.log(this.dataUser.length)
+            if(this.dataUser.length -1 === 0){
+              alert(true)
+              this.dataUser.splice(this.dataUser)
+            }
+            else {
+              alert(false)
+              this.fila = this.fila - 1;
+              const ask=prompt('Por favor ingrese el valor de la fila que desea eliminar')
+              this.dataUser.splice(ask)
+              console.log(ask)
+            }
+          }
     },
   })
 
